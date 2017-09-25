c71cbe598995e98fbcbcd01f22fbbb5dfdfeeead
Elisabeth L <elisabethsochacki@gmail.com>
2017-09-25 19:48:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ function whisper(string) {
   return string.toLowerCase()
 }
 var uppercase = "HELLO!"
+
 uppercase.toUpperCase()===uppercase//true
+
 var lowercase = 'hello!'
+
 lowercase.toLowerCase()===lowercase//true
 varmixedCase = 'Hi there!'
+
 mixedCase.toLowercase()===mixedCase//false
+
 mixedCase.toUpperCase()===mixedCase//false
